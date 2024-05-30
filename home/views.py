d5afba41bc12c5dab675dd0d662a5f4a37024388
Hossein siadati <hosseinsiadati01@gmail.com>
2024-05-31 00:36:58 +0330

    Home added (Not complete)

--- home/views.py
+++ home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def home_view(request):
-    return render(request, template_name="home/index.html")
+    return render(request, template_name="home/index-1.html")
