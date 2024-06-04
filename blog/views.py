f130311bed884fdd387c17a0355dffc821d6aa71
Hossein siadati <hosseinsiadati01@gmail.com>
2024-06-04 17:00:44 +0330

    BLOG AND BLOG-DETAILS ADDED

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,14 @@
-# from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Blog
-
-blog_details_template = 'blog_details.html'  # UPDATE IT
 
 
 def blog_list(request):
     blogs = Blog.objects.all()
     context = {'blogs': blogs}
-    return render(request, template_name='Blog/blog_list.html', context=context)
+    return render(request, template_name='blog/blog.html', context=context)
 
 
-# @login_required
-def blog_details(request, blog_id):
-    blog = Blog.objects.get(pk=blog_id)
+def blog_details(request, pk):
+    blog = Blog.objects.get(pk=pk)
     context = {'blog': blog}
-    return render(request, template_name=blog_details_template, context=context)
+    return render(request, template_name='blog/detail.html', context=context)
--- mine/urls.py
+++ mine/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -7,4 +9,7 @@ urlpatterns = [
     path("users/", include("users.urls")),
     path("", include('home.urls')),
     path("blog/", include('blog.urls')),
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
